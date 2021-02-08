a06ca9b2ddea2e80104ee8dbc4967421281b1189
vladdominator <73780858+vladdominator@users.noreply.github.com>
2021-02-08 04:44:03 +0300

    feat: for positive value

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+   if(n > 0){
+      let stringArray = String(n).split('').reverse().join('');
+      let numberValue = Number(stringArray);
+      return numberValue;
+   }
 }
